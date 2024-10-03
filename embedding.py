ffaca98cff35bec7930ee7048576767c9957d015
Prenom <baptiste.chachura@me.com>
2024-10-03 17:51:27 +0200

    push mais ca marche pas

--- embedding.py
+++ embedding.py
@@ -3,20 +3,39 @@ import numpy as np
 
 class Embedding : 
 
-    def __init__(self, len_voc: int, L : int ):
+    def __init__(self,  voc : dict, len_voc: int, L : int ):
         """
         L est la taille des embeddings
         """
-
-        
-        self.W = np.zeros((len_voc, L))
-        self.C = np.zeros(())
-
-    def Ppos():
-        return 
     
-    def Pneg():
-        return 
+        self.M = np.random.uniform(-0.1, 0.1, (len_voc, L))  # Vecteurs pour les mots cibles
+        self.C = np.random.uniform(-0.1, 0.1, (len_voc, L))  # Vecteurs pour les mots contextes
 
-    def loss():
-        return 
+    def sigmoid(self, x ): 
+        return 1 / (1+np.exp(-x))
+
+    def update_embeddings(self, m, cpos, cnegs, lr):
+        # Cpos
+        score_pos = np.dot(self.M[m], self.C[cpos])
+        grad_pos = self.sigmoid(score_pos) - 1
+        self.M[m] -= lr * grad_pos * self.C[cpos]
+        self.C[cpos] -= lr * grad_pos * self.M[m]
+
+        # Cneg
+        for cneg in cnegs:
+            score_neg = np.dot(self.M[m], self.C[cneg])
+            grad_neg = self.sigmoid(score_neg)
+            self.M[m] -= lr * grad_neg * self.C[cneg]
+            self.C[cneg] -= lr * grad_neg * self.M[m]
+
+
+
+    def generate( self, dataset, lr, it):
+        """
+        it = nombre d'iteration
+        lr = taux d'apprentissage
+        """
+        for epoch in range(it): 
+            for (m, cpos, cnegs) in dataset:  # dataset est constitué des exemples (positifs et négatifs)
+                self.update_embeddings(m, cpos, cnegs, lr)
+
--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import dataloader
 if __name__ == '__main__': 
     data = dataloader.Dataloader("Test/data/text_test.txt", 2, 3)
 
-    print("ok")
+    print( data.cneg)
     # charger donnée 
 
